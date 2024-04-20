50218e7a9592b0c6c7de6c564e53264e966e85bc
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-21 01:43:25 +0200

    responsiveness

--- src/components/shells/BentoShell.tsx
+++ src/components/shells/BentoShell.tsx
@@ -1,28 +1,28 @@
 import { HTMLAttributes, ReactNode } from "react";
 interface BentoBoxProps extends HTMLAttributes<HTMLDivElement> {
-  children: ReactNode;
-  bg?: string;
-  color?: string;
-  width?: "full" | "1/2" | "1/3" | "2/3" | "1/4" | "3/4";
-  noPadding?: boolean;
-  maxHeight?: boolean;
+    children: ReactNode;
+    bg?: string;
+    color?: string;
+    width?: "full" | "1/2" | "1/3" | "2/3" | "1/4" | "3/4";
+    noPadding?: boolean;
+    maxHeight?: boolean;
 }
 
 export default function BentoBox({
-  children,
-  bg = "card",
-  color = "neutral-200",
-  width = "full",
-  noPadding = false,
-  maxHeight = false,
-  ...props
+    children,
+    bg = "card",
+    color = "neutral-200",
+    width = "full",
+    noPadding = false,
+    maxHeight = false,
+    ...props
 }: BentoBoxProps) {
-  return (
-    <div
-      className={`font flex gap-[24px] flex-col ${noPadding ? "" : "p-24"} shadow-sm rounded-[30px] max-md:px-5 max-md:max-w-full bg-${bg} text-${color} w-${width} ${maxHeight ? "max-h-260" : ""}`}
-      {...props}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+            className={`font overflow-hidden flex gap-[24px] flex-col ${noPadding ? "" : "p-24"} shadow-sm rounded-[30px] max-md:px-5 max-md:max-w-full bg-${bg} text-${color} w-${width} ${maxHeight ? "max-h-260" : ""}`}
+            {...props}
+        >
+            {children}
+        </div>
+    );
 }
--- src/components/shells/PageShell.tsx
+++ src/components/shells/PageShell.tsx
@@ -1,21 +1,21 @@
 import Flex from "../core/Flexer";
 
 const PageShell = ({ children }) => {
-  return (
-    <Flex
-      as="main"
-      wrap="nowrap"
-      dir="col"
-      justify="start"
-      className="min-h-screen gap-24 p-page w-page relative mx-auto "
-    >
-      {children}
-    </Flex>
-  );
+    return (
+        <Flex
+            as="main"
+            wrap="nowrap"
+            dir="col"
+            justify="start"
+            className="min-h-screen gap-24 p-page md:w-page relative mx-auto "
+        >
+            {children}
+        </Flex>
+    );
 };
 
 const Container = ({ children }) => {
-  return <div className="w-page flex justify-center mx-auto">{children}</div>;
+    return <div className="md:w-page flex justify-center mx-auto">{children}</div>;
 };
 
 export default PageShell;
