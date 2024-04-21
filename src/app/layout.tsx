04981049c9f790912439d6460489917ab8a24f34
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-21 21:30:19 +0200

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,31 +3,31 @@ import "../styles/app.css";
 import ProviderWrapper from "@/core/providers/providers";
 import PageShell from "@/components/shells/PageShell";
 import TopmenuTwo from "@/components/menu/TopmenuTwo";
-import TopMenu from "@/components/menu/TopMenu";
-import TopMenuThree from "@/components/menu/TopMenuThree";
+import FooterShell from "@/components/shells/FooterShell";
 
 export const metadata: Metadata = {
-  title: "Remco Stoeten",
-  description: "Minimal landing page",
+    title: "Remco Stoeten",
+    description: "Minimal landing page",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="bg-body">
-        <ProviderWrapper>
-          <PageShell>
-            <>
-              <TopmenuTwo />
-              {children}
-            </>
-          </PageShell>
-        </ProviderWrapper>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className="bg-body">
+                <ProviderWrapper>
+                    <PageShell>
+                        <>
+                            <TopmenuTwo />
+                            {children}
+                            <FooterShell />
+                        </>
+                    </PageShell>
+                </ProviderWrapper>
+            </body>
+        </html>
+    );
 }
