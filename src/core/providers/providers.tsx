1a769ea553ca8698fa506864afcab236376f7879
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-17 14:50:45 +0200

    add posthog implementation

--- src/core/providers/providers.tsx
+++ src/core/providers/providers.tsx
@@ -2,14 +2,32 @@
 import { HydrationOverlay } from "@builder.io/react-hydration-overlay";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 export default function ProviderWrapper({ children }) {
+  if (typeof window !== "undefined") {
+    if (
+      !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
+      !process.env.NEXT_PUBLIC_POSTHOG_HOST
+    ) {
+      console.error(
+        "Environment variables NEXT_PUBLIC_POSTHOG_KEY and NEXT_PUBLIC_POSTHOG_HOST are not defined",
+      );
+    } else {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      });
+    }
+  }
+
   return (
-    
-    <HydrationOverlay>
-      {children}
-      <SpeedInsights />
-      <Analytics />
-    </HydrationOverlay>
+    <PostHogProvider client={posthog}>
+      <HydrationOverlay>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </HydrationOverlay>
+    </PostHogProvider>
   );
 }
