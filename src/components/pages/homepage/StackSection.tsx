8a6e5377179ecc99a2acb8c37e5acfd79270f8f8
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-17 18:15:27 +0200

    fix

--- src/components/pages/homepage/StackSection.tsx
+++ src/components/pages/homepage/StackSection.tsx
@@ -1,9 +1,11 @@
+'use client';
 import CTAButton from "@/components/core/Cta";
 import Paragraph from "@/components/core/Text";
 import { SkillIcon } from "@/components/core/icons";
 import BentoBox from "@/components/shells/BentoShell";
 import BentoTitle from "@/components/shells/BentoTitle";
 import { skillsData } from "@/core/data/skills";
+import { motion } from 'framer-motion';
 
 type StackSectionProps = {
   limit?: boolean;
@@ -12,39 +14,55 @@ type StackSectionProps = {
 export default function StackSection({ limit = false }: StackSectionProps) {
   const displayedSkills = limit ? skillsData.slice(0, 3) : skillsData;
 
+  const container = {
+    hidden: { opacity: 0 , scale: .8},
+    show: {
+      opacity: 1,
+        scale: 1,
+        transition: {
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const item = {
+    hidden: { opacity: 0, scale: 0.9 },
+    show: { opacity: 1, scale : 1 }
+  };
+
   return (
     <BentoBox>
       <BentoTitle icon={SkillIcon()}>Techstack</BentoTitle>
       <div className="flex flex-col gap-2">
         <Paragraph spacing="4">
-          I transitioned from Magento 2 (PHP) to a modern stack. Now, I
-          primarily use React, GraphQL, and Django at work. In my free time, I
-          build apps with NextJS and TypeScript. Besides that, I have dabbled a
+          I tra NextJS and TypeScript. Besides that, I have dabbled a
           little bit in Python, Lua, Svelte, and Bash. I find all technologies
           interesting and am planning on playing around with Go, eventually
           transitioning into full-stack (real full stack, not just JS + CRUD)."
         </Paragraph>
-        {displayedSkills.map((skill) => (
-          <>
-            <div className="flex items-center gap-4">
-              <div className="w-[68px] h-[68px] flex items-center justify-center bg-cardalt rounded-lg">
-                {skill.icon}
-              </div>
-              <div className="flex flex-col">
-                <h2 className="text-[16px]">{skill.name}</h2>
-                <p className="text-[14px] text-text">{skill.description}</p>
-              </div>
-            </div>
-            <div className="flex gap-4 pb-4">
-              <div className="seperator">
-                <div className="seperator__inner" />
-              </div>
-              <div className="text-[16px] text-text font-[300]">
-                {skill.paragraph}
-              </div>
-            </div>
-          </>
-        ))}
+  <motion.div variants={container} initial="hidden" animate="show">
+  {displayedSkills.map((skill) => (
+    <motion.div variants={item} key={skill.name}>
+      <div className="flex items-center gap-4">
+        <div className="w-[68px] h-[68px] flex items-center justify-center bg-cardalt rounded-lg">
+          {skill.icon}
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-[16px]">{skill.name}</h2>
+          <p className="text-[14px] text-text">{skill.description}</p>
+        </div>
+      </div>
+      <div className="flex gap-4 pb-4">
+        <div className="seperator">
+          <div className="seperator__inner" />
+        </div>
+        <div className="text-[16px] text-text font-[300]">
+          {skill.paragraph}
+        </div>
+      </div>
+    </motion.div>
+  ))}
+</motion.div>
         <CTAButton href="/my-stack" hasIcon>
           More about my stack
         </CTAButton>
