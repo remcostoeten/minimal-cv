b637f122092c99a9b8be7713fe10bff54601c913
Remco <57683378+remcostoeten@users.noreply.github.com>
2024-06-19 02:04:05 +0200

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,13 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next/dist/lib/metadata/types/metadata-interface'
+
+const getBaseUrl = 'https://remcostoeten.com'
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-  };
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/'
+        },
+        sitemap: `${getBaseUrl()}/sitemap.xml`
+    }
 }
