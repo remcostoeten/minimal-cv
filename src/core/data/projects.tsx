98a81ac9dc16d06bb70fe42c754d618890b8ee9f
Remco <57683378+remcostoeten@users.noreply.github.com>
2024-07-10 08:53:39 +0200

    Update projects.tsx

--- src/core/data/projects.tsx
+++ src/core/data/projects.tsx
@@ -1,42 +1,86 @@
 export const projectsData = [
   {
-    name: "MineSweeper",
-    description: "A replica of an online minesweeper game",
+    name: "Personal All-in-One Dashboard",
+    description: "A collection of tools I've built. WiP",
     icon: gameicon(),
     paragraph:
-      "This is a replica of the online casino game Dare 2 Mine by Hacksaw Gaming. I implemented a currency system which syncs all the data (bet size, wins, losses) to a database (Convex BaaS). The main core game is done, and the currency system is almost there. Most of the work that still needs to be done is UI, and basic game logic regarding the currency/game history.",
-    technologies: ["Typescript", "NextJS", "TailwindCSS", "Convex"],
-    link: "https://github.com/remcostoeten/minesweeper",
+      "A personal dashboard which (eventually) will contain all my tools I've built. HTML to TSX converter, (reverse) geo-location tool, SVG to pseudo-CSS, notes, kanban, file storage, experiments...",
+    technologies: ["TypeScript", "NextJS", "TailwindCSS", "SQLite", "Turso"],
+    link: "https://panel.remcostoeten.com",
   },
   {
-    name: "Whatsapp Status Scraper",
-    description: "An automated scraper for WhatsApp statuses",
-    icon: whatasppIcon(),
+    name: "Custom Commitizen Adapter",
+    description: "Makes it look like you know what you're doing",
+    icon: ToolIcon(),
+    paragraph: "A custom adapter for Commitizen to standardize commit messages.",
+    link: "https://www.npmjs.com/package/cleaner-commitizen-adapter",
+    technologies: [
+      "node",
+      "javascript",
+      "NPM",
+      "cli",
+    ],
+  },
+  {
+    name: "Password Manager Prototype",
+    description: "A prototype password manager",
+    icon: PasswordIcon(),
+    paragraph: "An assessment I've made. No back-end allowed, all local storage and not encrypted/hashed. Purely an MVP prototype.",
+    link: "https://password-manager.remcostoeten.com/",
+    technologies: [
+      "NextJS 15 RC",
+      "React 19",
+      "Local Storage",
+      "Api",
+      "Server actions",
+      ],
+  },
+  {
+    name: "WhatsApp Status Scraper",
+    description: "Rework postponed: An automated scraper for WhatsApp statuses",
+    icon: whatsappicon(),
     paragraph:
       "No, I don't condone stalking, but I love exploring new technologies. I've built a Python Selenium Chromedriver WhatsApp status scraper but wanted more UI. Thus, I ported it over to a NextJS API. Scrapable settings are configurable via a config file, launches up Chromedriver, navigates to WhatsApp Web, and starts monitoring the online status which then gets saved to local objects and displayed nicely in real-time in a table UI.",
     technologies: [
-      "Typescript",
+      "TypeScript",
       "NextJS",
       "TailwindCSS",
       "Python",
       "Selenium",
       "Chromedriver",
     ],
-    link: "https://github.com/remcostoeten/personal-platform/blob/master/pages/api/status/index.ts#L1",
+    link: "https://github.com/remcostoeten/personal-platform/blob/master/pages/api/status/index.ts#L113",
   },
 ];
 
-function whatasppicon() {
+function ToolIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <path d="M14.74,9.35L15,9.59L15.23,9.34A5.84,5.84,0,0,0,18,4.92V4A1,1,0,0,0,17,3H14.06a1,1,0,0,0-.89.55L12.67,5.6a1,1,0,0,0,.2,1.11l1.86,1.86A7.81,7.81,0,0,0,14.74,9.35ZM20.29,3.71a1,1,0,0,0-1.42,0l-.93.93a7.82,7.82,0,0,0-6.36-3.28,7.83,7.83,0,0,0-7.79,7.86,7.93,7.93,0,0,0,4.24,7.11L1.67,20.93a2.62,2.62,0,0,0,0,3.71,2.62,2.62,0,0,0,3.71,0l6.37-6.37a7.92,7.92,0,0,0,7.11,4.24A7.83,7.83,0,0,0,23,9.93a7.82,7.82,0,0,0-3.28-6.36l.93-.93A1,1,0,0,0,20.29,3.71Z"/>
+</svg>
+
+  )
+}
+  function PasswordIcon() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <path d="M12 2C8.14 2 5 5.14 5 9v3H4a2 2 0 00-2 2v6a2 2 0 002 2h16a2 2 0 002-2v-6a2 2 0 00-2-2h-1V9c0-3.86-3.14-7-7-7zm-4 9V9a4 4 0 018 0v2H8zm10 2h-4v4h4v-4zm-6 0H6v4h6v-4z"/>
+</svg>
+
+
+    )
+}
+function whatsappicon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
-      height="24   "
+      height="24"
       viewBox="0 0 24 24"
     >
       <path
         fill="#fff"
-        d="motion.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"
+        d="M14.9978834,5 C18.8638767,5 21.9978834,8.13400675 21.9978834,12 C21.9978834,15.7854517 18.9931001,18.8690987 15.2385332,18.995941 L14.9978834,19 L9.00211656,19 C5.13612331,19 2.00211656,15.8659932 2.00211656,12 C2.00211656,8.21454828 5.00689994,5.13090132 8.76146681,5.00405902 L9.00211656,5 L14.9978834,5 Z M14.75,12.5 C14.0596441,12.5 13.5,13.0596441 13.5,13.75 C13.5,14.4403559 14.0596441,15 14.75,15 C15.4403559,15 16,14.4403559 16,13.75 C16,13.0596441 15.4403559,12.5 14.75,12.5 Z M8,9 C7.62030423,9 7.30650904,9.28215388 7.25684662,9.64822944 L7.25,9.75 L7.25,11.248 L5.75,11.2487458 C5.33578644,11.2487458 5,11.5845322 5,11.9987458 C5,12.3784415 5.28215388,12.6922367 5.64822944,12.7418991 L5.75,12.7487458 L7.25,12.748 L7.25,14.25 C7.25,14.6642136 7.58578644,15 8,15 C8.37969577,15 8.69349096,14.7178461 8.74315338,14.3517706 L8.75,14.25 L8.75,12.748 L10.25,12.7487458 C10.6642136,12.7487458 11,12.4129593 11,11.9987458 C11,11.61905 10.7178461,11.3052548 10.3517706,11.2555924 L10.25,11.2487458 L8.75,11.248 L8.75,9.75 C8.75,9.33578644 8.41421356,9 8,9 Z M16.75,9 C16.0596441,9 15.5,9.55964406 15.5,10.25 C15.5,10.9403559 16.0596441,11.5 16.75,11.5 C17.4403559,11.5 18,10.9403559 18,10.25 C18,9.55964406 17.4403559,9 16.75,9 Z"
       />
     </svg>
   );
@@ -61,28 +105,8 @@ function gameicon() {
         <g id="ic_fluent_games_24_filled" fill="#fff" fillRule="nonzero">
           <path
             d="M14.9978834,5 C18.8638767,5 21.9978834,8.13400675 21.9978834,12 C21.9978834,15.7854517 18.9931001,18.8690987 15.2385332,18.995941 L14.9978834,19 L9.00211656,19 C5.13612331,19 2.00211656,15.8659932 2.00211656,12 C2.00211656,8.21454828 5.00689994,5.13090132 8.76146681,5.00405902 L9.00211656,5 L14.9978834,5 Z M14.75,12.5 C14.0596441,12.5 13.5,13.0596441 13.5,13.75 C13.5,14.4403559 14.0596441,15 14.75,15 C15.4403559,15 16,14.4403559 16,13.75 C16,13.0596441 15.4403559,12.5 14.75,12.5 Z M8,9 C7.62030423,9 7.30650904,9.28215388 7.25684662,9.64822944 L7.25,9.75 L7.25,11.248 L5.75,11.2487458 C5.33578644,11.2487458 5,11.5845322 5,11.9987458 C5,12.3784415 5.28215388,12.6922367 5.64822944,12.7418991 L5.75,12.7487458 L7.25,12.748 L7.25,14.25 C7.25,14.6642136 7.58578644,15 8,15 C8.37969577,15 8.69349096,14.7178461 8.74315338,14.3517706 L8.75,14.25 L8.75,12.748 L10.25,12.7487458 C10.6642136,12.7487458 11,12.4129593 11,11.9987458 C11,11.61905 10.7178461,11.3052548 10.3517706,11.2555924 L10.25,11.2487458 L8.75,11.248 L8.75,9.75 C8.75,9.33578644 8.41421356,9 8,9 Z M16.75,9 C16.0596441,9 15.5,9.55964406 15.5,10.25 C15.5,10.9403559 16.0596441,11.5 16.75,11.5 C17.4403559,11.5 18,10.9403559 18,10.25 C18,9.55964406 17.4403559,9 16.75,9 Z"
-            id="🎨-Color"
           ></path>
         </g>
-      </g>
-    </svg>
-  );
-}
-
-function whatasppIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={28}
-      height={28}
-      fill="white"
-      version="1.1"
-      viewBox="0 0 308 308"
-      xmlSpace="preserve"
-    >
-      <g>
-        <path d="M227.904 176.981c-.6-.288-23.054-11.345-27.044-12.781-1.629-.585-3.374-1.156-5.23-1.156-3.032 0-5.579 1.511-7.563 4.479-2.243 3.334-9.033 11.271-11.131 13.642-.274.313-.648.687-.872.687-.201 0-3.676-1.431-4.728-1.888-24.087-10.463-42.37-35.624-44.877-39.867-.358-.61-.373-.887-.376-.887.088-.323.898-1.135 1.316-1.554 1.223-1.21 2.548-2.805 3.83-4.348a140.77 140.77 0 011.812-2.153c1.86-2.164 2.688-3.844 3.648-5.79l.503-1.011c2.344-4.657.342-8.587-.305-9.856-.531-1.062-10.012-23.944-11.02-26.348-2.424-5.801-5.627-8.502-10.078-8.502-.413 0 0 0-1.732.073-2.109.089-13.594 1.601-18.672 4.802C90 87.918 80.89 98.74 80.89 117.772c0 17.129 10.87 33.302 15.537 39.453.116.155.329.47.638.922 17.873 26.102 40.154 45.446 62.741 54.469 21.745 8.686 32.042 9.69 37.896 9.69h.001c2.46 0 4.429-.193 6.166-.364l1.102-.105c7.512-.666 24.02-9.22 27.775-19.655 2.958-8.219 3.738-17.199 1.77-20.458-1.348-2.216-3.671-3.331-6.612-4.743z"></path>
-        <path d="M156.734 0C73.318 0 5.454 67.354 5.454 150.143c0 26.777 7.166 52.988 20.741 75.928L.212 302.716a3.998 3.998 0 004.999 5.096l79.92-25.396c21.87 11.685 46.588 17.853 71.604 17.853C240.143 300.27 308 232.923 308 150.143 308 67.354 240.143 0 156.734 0zm0 268.994c-23.539 0-46.338-6.797-65.936-19.657a3.996 3.996 0 00-3.406-.467l-40.035 12.726 12.924-38.129a4.002 4.002 0 00-.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677 0-65.543 53.754-118.867 119.826-118.867 66.064 0 119.812 53.324 119.812 118.867.001 65.535-53.746 118.851-119.811 118.851z"></path>
       </g>
     </svg>
   );
